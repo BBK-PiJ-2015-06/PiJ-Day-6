8c0ad16a9d92835f62d40e54276a0e617c072417
unknown <gshian01@CSPC021.dcs.bbk.ac.uk>
2015-11-19 10:39:30 +0000

    Solution to exercise 1 completed

---
+++ Exercise 1 - Dividing Integers/TestScript.groovy
@@ -0,0 +1,6 @@
+Calculator myCalculator = new Calculator();
+println myCalculator.add(2, 5);
+println myCalculator.subtract(2, 5);
+println myCalculator.multiply(2, 5);
+println myCalculator.divide(2, 5);
+println myCalculator.modulus(2, 5);
